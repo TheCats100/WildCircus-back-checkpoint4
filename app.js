717208ef2615a9d98b5105f8a803ad2168b877f8
TheCats100 <m-g-152@outlook.fr>
2020-07-22 12:25:01 +0200

    feat(router): first route performances

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
+const cors = require('cors')
+const indexRouter = require('./routes');
 
 app.use(cors())
 app.use(express.json());
@@ -8,6 +12,10 @@ app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.static('public'));
+
+
+app.use('/', indexRouter);
+
 
 app.get('/', (request, response) => {
     response.json({message: 'Bienvenue sur Express'} );
