fe9cb6c28b744802c09a11db058e900d42d53ac3
TheCats100 <m-g-152@outlook.fr>
2020-07-22 12:14:12 +0200

    feat(app): app use usefull

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
+
 app.get('/', (request, response) => {
     response.json({message: 'Bienvenue sur Express'} );
   });
